8f7a7bbffa716751e16dc8fe85532dd16727c7a0
F80DON <fatih.donat@outlook.de>
2023-07-28 12:48:15 +0200

    Collaborator commit

--- src/Student.java
+++ src/Student.java
@@ -2,8 +2,7 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("Pink Floyd");
         System.out.println("Another brick in the wall");
-
-        System.out.println("It's me the collaborator");
+        System.out.println("collab worker");
 
     }
 }
